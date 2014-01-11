b4d36adc901b8092b99b82f8a7a3c379278d8b54
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2014-01-11 22:16:30 +0800

    0.1.0 release

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
-(defproject lein-dynalint "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject lein-dynalint "0.1.0"
+  :description "A Leiningen plugin for Dynalint"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.ambrosebs/dynalint "0.1.0-SNAPSHOT"]]
+                 [com.ambrosebs/dynalint "0.1.0"]]
   :eval-in-leiningen true)

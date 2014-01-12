9a70da6da4a0955f2d4accc19f43ac616087a3a5
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2014-01-12 23:18:21 +0800

    dynalint 0.1.1 dep

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.ambrosebs/dynalint "0.1.1-SNAPSHOT"]]
+                 [com.ambrosebs/dynalint "0.1.1"]]
   :eval-in-leiningen true)

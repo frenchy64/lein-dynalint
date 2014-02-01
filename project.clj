f2baea43307a21c8c2d55fe13efc0d93a4be6ec2
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2014-02-01 21:15:06 +0800

    0.1.2

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject lein-dynalint "0.1.1"
+(defproject lein-dynalint "0.1.2"
   :description "A Leiningen plugin for Dynalint"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.ambrosebs/dynalint "0.1.1"]]
+                 [com.ambrosebs/dynalint "0.1.2"]]
   :eval-in-leiningen true)
